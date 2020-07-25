d00a5c898e827883e55dee252ed2efd392585cb8
Thato-Raphahlelo <68586559+Thato-Raphahlelo@users.noreply.github.com>
2020-07-25 16:14:43 +0200

    Weather PWA

---
+++ build/precache-manifest.111b4908bb4f6823670874c339e6d2e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b02095d45068ec995276e4049e156be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1491fccd605231351f6",
+    "url": "/static/css/main.e7dadc21.chunk.css"
+  },
+  {
+    "revision": "d93b36bd7c1e069ca6cd",
+    "url": "/static/js/2.3c7f5287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c7f5287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1491fccd605231351f6",
+    "url": "/static/js/main.f7bfa343.chunk.js"
+  },
+  {
+    "revision": "0449b321fbcdc3e754f9",
+    "url": "/static/js/runtime-main.75a65c2f.js"
+  }
+]);
